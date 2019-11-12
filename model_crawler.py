6f46f4fbd4873a546ceb2156d802a93a426e03b2
Unknown <cookkyh99@skku.edu>
2019-11-12 22:09:08 +0900

    crawl model personal data

--- model_crawler.py
+++ model_crawler.py
@@ -1,19 +1,17 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 
 driver = webdriver.Chrome('/Users/deinm/Downloads/chromedriver')
 driver.implicitly_wait(3)
 
-URL = 'https://models.com/rankings/ui/Runway/'
+# scroll down
+'''
+URL = 'https://models.com/rankings/ui/infinite/Runway-All/'
 driver.get(URL)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 elem = driver.find_element_by_tag_name('body')
-print(elem)
 elem.send_keys(Keys.END)
 
 pagedowns = 1
@@ -25,15 +23,70 @@ while pagedowns < 72:
         # 페이지 스크롤 타이밍을 맞추기 위해 sleep
         time.sleep(1)
         pagedowns+=1
+'''
 
-names = soup.find_all('div',{'class':'small-12 medium-3 columns showtotals'})
+base_URL = 'https://models.com/rankings/ui/infinite/Runway-All/'
 
-for name in names:
-    single_name = name.find('h3').text
-    data = name.find_all('li')
-    walked = data[0].text
-    oc_num = 'None'
-    if len(data)==2:
-        oc_num = data[1].text
-    print(single_name,walked,oc_num)
-    print("@")
+model_namelist = []
+model_walklist = []
+model_ocnumlist = []
+model_nationallist = []
+model_personallist = []
+
+for i in range(1,241):
+    URL = base_URL + str(i)
+    driver.get(URL)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    names = soup.find_all('div',{'class':'small-12 medium-3 columns showtotals'})
+
+    model_url = []
+
+    for name in names:
+        model_url.append(name.find('h3').find('a')['href'])
+        single_name = name.find('h3').text
+        data = name.find_all('li')
+        walked = data[0].text
+        oc_num = 'None'
+        if len(data)==2:
+            oc_num = data[1].text
+
+        model_namelist.append(single_name)
+        model_walklist.append(walked)
+        model_ocnumlist.append(oc_num)
+
+    for single_url in model_url:
+        driver.get(single_url)
+        driver.implicitly_wait(1)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        nationality = 'None'
+        try:
+            nationality = soup.find('div',{'id':'biodata'}).find('div').text
+        except:
+            pass
+
+        model_nationallist.append(nationality)
+
+        personal_data = []
+        personal_head = []
+
+        try:
+            personal_head = soup.find('table',{'id':'cmtable'}).find_all('th')
+            tmp_data = soup.find('table',{'id':'cmtable'}).find_all('td')
+            for single_data in tmp_data:
+                personal_data.append(single_data.text)
+        except:
+            pass
+
+        personal_dict = {}
+        length = len(personal_head)
+        if length != 0:
+            for index in range(length):
+                personal_dict[personal_head[index].text] = personal_data[index]
+
+        model_personallist.append(personal_dict)
+
+    print(model_personallist)
